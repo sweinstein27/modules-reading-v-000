b280966eca7ad903017eb00926d313cf3d806e71
Scott Weinstein <sweinstein27@gmail.com>
2018-05-29 12:37:49 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,22 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Dancer
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
